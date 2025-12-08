d8c66d1742413a9ac5e60e0c3b6e901c73eda870
pravinsamr1 <pravinsamr@gmail.com>
2025-12-08 14:17:58 +0530

    dd

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-  
-app.use(cors(corsOptions));    
 app.use(express.json())
 app.use(cookieParser())  // ONLY ONCE
 
